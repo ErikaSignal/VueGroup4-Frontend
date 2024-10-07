3c8d363244eafc591275c92a90b60a7d111a1d12
Erika Signal <erika.signal@gmail.com>
2024-10-07 17:48:02 +0200

    Add Bootstrap to code

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="homepage">
-        <header>
-            <img :src="logo" alt="Cinema Logo" class="logo" />
+        <header class="text-center mb-4">
+            <img :src="logo" alt="Cinema Logo" class="logo img-fluid" />
         </header>
 
         <div class="movie-poster-header">
             <h3>Filmer vi visar nu</h3>
         </div>
-        <section class="movies-grid">
-            <div class="movie-poster" v-for="movie in movies" :key="movie.id">
+
+        <div class="row movies-grid">
+            <div class="col-md-4 movie-poster" v-for="movie in movies" :key="movie.id">
                 <router-link :to="{ name: 'MoviePage', params: { id: movie.id } }">
-                    <img :src="movie.posterUrl" :alt="movie.title" />
+                    <img :src="movie.posterUrl" :alt="movie.title" class="img-fluid" />
                 </router-link>
                 <h3>
                     <router-link :to="{ name: 'MoviePage', params: { id: movie.id } }">
@@ -18,31 +19,46 @@
                     </router-link>
                 </h3>
             </div>
-        </section>
+        </div>
     </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue';
 import poster1 from '/src/img/poster1.jpg';
 import poster2 from '/src/img/poster2.jpg';
 import poster3 from '/src/img/poster3.jpg';
 
-export default {
-    setup() {
-        const logo = './src/img/logo.png';
+const logo = './src/img/logo.png';
 
-        const movies = [
-            { id: 1, title: 'Movie 1', posterUrl: poster1 },
-            { id: 2, title: 'Movie 2', posterUrl: poster2 },
-            { id: 3, title: 'Movie 3', posterUrl: poster3 }
-        ];
+const movies = ref([
+    { id: 1, title: 'Movie 1', posterUrl: poster1 },
+    { id: 2, title: 'Movie 2', posterUrl: poster2 },
+    { id: 3, title: 'Movie 3', posterUrl: poster3 }
+]);
 
-        return {
-            logo,
-            movies
-        };
+const fetchMovies = async () => {
+    const apiUrls = [
+        'https://www.swapi.tech/api/films/1',
+        'https://www.swapi.tech/api/films/2',
+        'https://www.swapi.tech/api/films/3'
+    ];
+
+    try {
+        const responses = await Promise.all(apiUrls.map(url => fetch(url)));
+        const data = await Promise.all(responses.map(res => res.json()));
+
+        movies.value[0].title = data[0].result.properties.title;
+        movies.value[1].title = data[1].result.properties.title;
+        movies.value[2].title = data[2].result.properties.title;
+    } catch (error) {
+        console.error('Fel vid hämtning av filmer:', error);
     }
 };
+
+onMounted(() => {
+    fetchMovies();
+});
 </script>
 
 <style scoped>
