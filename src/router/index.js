f92804311fc439682f73c08d31774200ca52310b
Hanna Stigland <hanna.stigland@iths.se>
2024-10-09 10:01:48 +0200

    Merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '/src/views/HomePage.vue';
 import MoviePage from '/src/views/MoviePage.vue';
+import Infopage1 from '/src/views/infopage1.vue';
 
 const routes = [
   {
