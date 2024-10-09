7cae063761e129b106beff4f8a6614aa147daa10
Yrla Hackzell <yrla.hackzell@hotmail.com>
2024-10-09 10:26:48 +0200

    routing

--- src/views/infopage1.vue
+++ src/views/infopage1.vue
@@ -7,22 +7,22 @@
   
       <!-- Film Information Section -->
       <section class="container-lg">
-      <div class="row">
+      <div v-if="post" class="row">
        
           <img class="col-md-3 col-12 " src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png" alt="Movie Poster" width="200">
     
     <div class="col-md col-12 d-flex flex-column">
-    <h2>Titel</h2>
+    <h2>{{ post.title }}</h2>
     <div class="flex-grow-1">
-    <p class="fs-4">Info om filmen</p> 
+    <p class="fs-4">{{ post.opening_crawl }}</p> 
     </div>
      </div>
 <div class="col-md col-11 d-flex flex-column custom-border mx-auto custom-bg">
   <div style="height: 2.5rem;"></div> 
   <div class="flex-grow-1">
-          <p class="fs-4 fw-bold">Utgivningsdatum:</p>
-          <p class="fs-4 fw-bold">Producent:</p>
-          <p class="fs-4 fw-bold">Regissör:</p>
+          <p class="fs-4 fw-bold">Utgivningsdatum: {{ post.release_date }}</p>
+          <p class="fs-4 fw-bold">Producent: {{ post.producer }}</p>
+          <p class="fs-4 fw-bold">Regissör: {{ post.director }}</p>
         </div>
       </div>
     </div>
@@ -37,7 +37,7 @@
             <img class="col-md col-12 p-4 " src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png" alt="Movie Poster" height="180">
          
           <div class="col-md col-12 d-flex flex-column justify-content-center align-items-center" >
-            <p class="fs-2">Titel</p>
+            <p class="fs-2">{{post.title}}</p>
             <p class="fs-4">Kl 18:00</p>
             
           </div>
@@ -58,7 +58,7 @@
             <img class="col-md col-12 p-4 " src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png" alt="Movie Poster" height="180">
          
           <div class="col-md col-12 d-flex flex-column justify-content-center align-items-center" >
-            <p class="fs-2">Titel</p>
+            <p class="fs-2">{{post.title}}</p>
             <p class="fs-4">Kl 19:15</p>
             
           </div>
@@ -76,8 +76,31 @@
     </div>
   </template>
   
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { getOneMovie } from '../services/api.js'
+const route = useRoute()
+const post = ref(null)
+const error = ref(null)
+const loading = ref(false)
+watch(() => route.params.id, fetchData, { immediate: true })
+async function fetchData() {
+  loading.value = true
+  post.value = null
+  error.value = null
+  try {
+    post.value = await getOneMovie(route.params.id) 
+  } catch (err) {
+    error.value = err.toString()
+  } finally {
+    loading.value = false
+  }
+}
+</script>
   <script>
   import { RouterLink, RouterView } from 'vue-router'
+
   export default {
     name: "MovieBooking",
     data() {
