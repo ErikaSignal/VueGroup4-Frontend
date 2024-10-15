d49de571efbbe6326dca23380e1ea5e544d0261a
Erika Signal <erika.signal@gmail.com>
2024-10-15 11:19:19 +0200

    Update booking with correct id

--- src/views/infopage1.vue
+++ src/views/infopage1.vue
@@ -1,85 +1,72 @@
 <template>
-    <div class="background">
-      
-      <header class="d-flex justify-content-center justify-content-md-start pb-5">
-        <RouterLink to="/"><img src="@/assets/logo.png" alt="logo" width="200"></RouterLink>
-      </header>
+  <div class="background">
 
-      <!-- Film Information Section -->
-      <section class="container-lg">
+    <header class="d-flex justify-content-center justify-content-md-start pb-5">
+      <RouterLink to="/"><img src="@/assets/logo.png" alt="logo" width="200"></RouterLink>
+    </header>
+
+    <!-- Film Information Section -->
+    <section class="container-lg">
       <div v-if="post" class="row">
-       
-          <img class="col-md-3 col-12 " src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png" alt="Movie Poster" width="200">
-    
-    <div class="col-md col-12 d-flex flex-column">
-    <h2>{{ post.title }}</h2>
-    <div class="flex-grow-1">
-    <p class="fs-4">{{ post.opening_crawl }}</p> 
-    </div>
-     </div>
-<div class="col-md col-11 d-flex flex-column custom-border mx-auto custom-bg">
-  <div style="height: 2.5rem;"></div> 
-  <div class="flex-grow-1">
-          <p class="fs-4 fw-bold">Utgivningsdatum: {{ post.release_date }}</p>
-          <p class="fs-4 fw-bold">Producent: {{ post.producer }}</p>
-          <p class="fs-4 fw-bold">Regissör: {{ post.director }}</p>
+
+        <img class="col-md-3 col-12 "
+          src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png"
+          alt="Movie Poster" width="200">
+
+        <div class="col-md col-12 d-flex flex-column">
+          <h2>{{ post.title }}</h2>
+          <div class="flex-grow-1">
+            <p class="fs-4">{{ post.opening_crawl }}</p>
+          </div>
+        </div>
+        <div class="col-md col-11 d-flex flex-column custom-border mx-auto custom-bg">
+          <div style="height: 2.5rem;"></div>
+          <div class="flex-grow-1">
+            <p class="fs-4 fw-bold">Utgivningsdatum: {{ post.release_date }}</p>
+            <p class="fs-4 fw-bold">Producent: {{ post.producer }}</p>
+            <p class="fs-4 fw-bold">Regissör: {{ post.director }}</p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  
-      <!-- Visning Information Section -->
-      <section class="container py-4 ">
-        <h3>Torsdag 10 Oktober</h3>
-        <div class="row custom-height col-md col-11 m-auto rounded border mb-5 pb-md-0 pb-4 w-100">
-          
-            <img class="col-md col-12 p-4 " src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png" alt="Movie Poster" height="180">
-         
-          <div class="col-md col-12 d-flex flex-column justify-content-center align-items-center" >
-            <p  v-if="post" class="fs-2">{{ post.title }}</p>
-            <p class="fs-4">Kl 18:00</p>
-            
-          </div>
-          <div class="col-md col-12 d-flex justify-content-center align-items-center" >
-          <!--  <p class="fs-4">Platser kvar: {{ totalSeats-bookedSeats }} av {{ totalSeats }}</p> -->
-          </div>
-          <div class="col-md col-12 d-flex justify-content-center align-items-center" >
 
-         <ModalComponent/> 
+    <!-- Visning Information Section -->
+    <section v-for="(film, index) in filmer" :key="index" class="container py-4 ">
 
-          </div>
-
-      </div>
       <h3>Onsdag 16 Oktober</h3>
-            
-          <div   class="row custom-height col-md col-11 m-auto rounded border mb-5 pb-md-0 pb-4 w-100">
-          
-            <img class="col-md col-12 p-4 " src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png" alt="Movie Poster" height="180">
-         
-          <div class="col-md col-12 d-flex flex-column justify-content-center align-items-center" >
-            <p v-if="post" class="fs-2">{{post.title}}</p>
-            <p class="fs-4">Kl 19:15</p>
-            
-          </div>
-          <div class="col-md col-12 d-flex justify-content-center align-items-center" >
-        <p v-if="filmer.length > 0" class="fs-5">
-  Platser kvar: {{ filmer[0].totalSeats - filmer[0].bookedSeats }} av {{ filmer[0].totalSeats }}
-</p>
-<p v-else>
-  Laddar bokningsinformation...
-</p>      </div>
-          <div class="col-md col-12 d-flex justify-content-center align-items-center" >
 
-            <ModalComponent/> 
+      <div  class="row custom-height col-md col-11 m-auto rounded border mb-5 pb-md-0 pb-4 w-100">
 
-          </div>
+        <img class="col-md col-12 p-4 "
+          src="https://www.emp-shop.se/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw79144a0d/images/5/2/4/3/524369a.jpg?sw=350&sh=400&sm=fit&sfrm=png"
+          alt="Movie Poster" height="180">
+
+        <div class="col-md col-12 d-flex flex-column justify-content-center align-items-center">
+          <p v-if="post" class="fs-2">{{ post.title }}</p>
+          <p class="fs-4">Kl 19:15</p>
+
+        </div>
+        <div 
+          class="col-md col-12 d-flex justify-content-center align-items-center">
+          <p v-if="filmer.length > 0" class="fs-5">
+            Platser kvar: {{ film.totalSeats - film.bookedSeats }} av {{ film.totalSeats }}
+          </p>
+          <p v-else>
+            Laddar bokningsinformation...
+          </p>
+        </div>
+        <div class="col-md col-12 d-flex justify-content-center align-items-center">
+
+          <ModalComponent />
+
+        </div>
 
       </div>
-      </section>
-    </div>
-  </template>
-  
+    </section>
+  </div>
+</template>
+
 <script setup>
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
@@ -93,19 +80,19 @@ axios.defaults.baseURL = 'http://localhost:8081/api';
 const filmer = ref([]);
 
 
-    const fetchFilmer = async () => {
-      try {
-        const response = await axios.get('/booking');
-        filmer.value = response.data;  
-        console.log("Filmer:", filmer.value);
-      } catch (error) {
-        console.error("Kunde inte hämta filmer:", error);
-      }
-    };
+const fetchFilmer = async () => {
+  try {
+    const response = await axios.get(`/swapi/${route.params.id}`);
+    filmer.value = response.data;
+    console.log("Filmer:", filmer.value);
+  } catch (error) {
+    console.error("Kunde inte hämta filmer:", error);
+  }
+};
 
-    onMounted(() => {
-      fetchFilmer();  
-    });
+onMounted(() => {
+  fetchFilmer();
+});
 
 const route = useRoute()
 const post = ref(null)
@@ -117,7 +104,7 @@ async function fetchData() {
   post.value = null
   error.value = null
   try {
-    post.value = await getOneMovie(route.params.id) 
+    post.value = await getOneMovie(route.params.id)
   } catch (err) {
     error.value = err.toString()
   } finally {
@@ -125,8 +112,8 @@ async function fetchData() {
   }
 }
 </script>
-  <script>
-  import { RouterLink, RouterView } from 'vue-router'
+<script>
+import { RouterLink, RouterView } from 'vue-router'
 /*  export default {
     name: "MovieBooking",
     data() {
@@ -147,14 +134,14 @@ async function fetchData() {
     },
   };
   */
-  </script>
-  
-  <style scoped>
-  .mt-2{
-    margin-bottom:3rem;
-  }
+</script>
 
-  .background {
+<style scoped>
+.mt-2 {
+  margin-bottom: 3rem;
+}
+
+.background {
   background-image: url('@/assets/bg-pic3.png');
   background-size: cover;
   background-position: center;
@@ -162,15 +149,14 @@ async function fetchData() {
   min-width: 100%;
   color: white;
   padding: 0;
-  }
+}
 
-  .custom-border {
+.custom-border {
   border-radius: 30px;
-  }
+}
 
 
-  .custom-bg {
-    background-color: rgba(117, 118, 126, 0.35);
-  }
-  </style>
-  
+.custom-bg {
+  background-color: rgba(117, 118, 126, 0.35);
+}
+</style>
