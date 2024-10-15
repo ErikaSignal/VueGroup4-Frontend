2fb3b764f043c0218175580360fc0cb76bc8d02a
Erika Signal <erika.signal@gmail.com>
2024-10-15 10:00:25 +0200

    Add server port

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    port: 5174,  // Här sätter du portnumret
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
