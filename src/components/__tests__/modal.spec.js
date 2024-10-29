03ade565beea7c91cbb2c6a89315bc7aa23d4216
Hanna Stigland <hanna.stigland@iths.se>
2024-10-29 09:41:43 +0100

    removed unused imports

--- src/components/__tests__/modal.spec.js
+++ src/components/__tests__/modal.spec.js
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils';
-import { describe, it, expect, vi } from 'vitest';
+
+import { describe, it, expect, vi} from 'vitest';
+import { mount } from '@vue/test-utils'
 import Modal from '@/components/Modal.vue';
 import infopage1 from '@/views/infopage1.vue';
 import { fetchFilmer } from '@/services/database';
 import { getOneMovie } from '@/services/api';
-
 
 vi.mock('@/services/database', () => ({
   fetchFilmer: vi.fn(),
@@ -54,3 +54,52 @@ describe('infopage1', () => {
     expect(wrapper.vm.requestedSeats).toBe(1);
     expect(wrapper.vm.email).toBe('');
   });
+
+  /// increase and decrease seatvalue 
+
+describe('Modal', () => {
+    it('increases tickets when increaseTickets is called', async () => {
+        document.body.innerHTML = `<div id="exampleModalCenter"></div>`;
+        const wrapper = mount(Modal, {
+          props: {
+            title: 'Sample Movie',
+            filmId: 1,
+          },
+        });
+        const ticketInput = wrapper.find('#requestedSeats');
+
+        expect(ticketInput.element.value).toBe('1');
+    
+        const increaseButton = wrapper.find('button.btn-outline-secondary[type="button"]:last-child');
+        await increaseButton.trigger('click');
+    
+        expect(ticketInput.element.value).toBe('2');
+      });
+    });
+    
+    describe('Modal', () => {
+        it('decreases tickets when decreaseTickets is called', async () => {
+            document.body.innerHTML = `<div id="exampleModalCenter"></div>`;
+            const wrapper = mount(Modal, {
+              props: {
+                title: 'Sample Movie',
+                filmId: 1,
+              },
+            });
+            const ticketInput = wrapper.find('#requestedSeats');
+    
+            expect(ticketInput.element.value).toBe('1');
+        
+            const increaseButton = wrapper.find('button.btn-outline-secondary[type="button"]:last-child');
+            const decreaseButton = wrapper.find('button.btn-outline-secondary[type="button"]:first-child');
+            await increaseButton.trigger('click');
+        
+            expect(ticketInput.element.value).toBe('2');
+            await decreaseButton.trigger('click');
+            expect(ticketInput.element.value).toBe('1');
+          });
+        });
+        
+     
+
+        // email input works and does not work 
