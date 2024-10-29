14d245015610567b5b6e46738879ad386bc7b210
Yrla Hackzell <yrla.hackzell@hotmail.com>
2024-10-29 11:17:00 +0100

    removed images

--- src/services/utils/json.js
+++ src/services/utils/json.js
@@ -1,15 +1,9 @@
 import poster1 from '/src/assets/img/poster1.jpg';
 import poster2 from '/src/assets/img/poster2.jpg';
 import poster3 from '/src/assets/img/poster3.jpg';
-import poster4 from '/src/assets/img/poster4.jpg';
-import poster5 from '/src/assets/img/poster5.jpg';
-import poster6 from '/src/assets/img/poster6.jpg';
 
   export const posters = [
     { id: 1, title: 'Movie 1', posterUrl: poster1 },
     { id: 2, title: 'Movie 2', posterUrl: poster2 },
     { id: 3, title: 'Movie 3', posterUrl: poster3 },
-    // { id: 4, title: 'Movie 4', posterUrl: poster4 },
-    // { id: 5, title: 'Movie 5', posterUrl: poster5 },
-    // { id: 6, title: 'Movie 6', posterUrl: poster6 }
 ];
